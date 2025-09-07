3f5ced75ff2985ae87e5502ab29cdc67b5bf0fb6
AhanaSaraff <ahanasaraff@gmail.com>
2025-09-07 22:25:36 +0530

    Details

--- notes/lib/add_note_page.dart
+++ notes/lib/add_note_page.dart
@@ -1,1 +1,17 @@
-class 
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class AddNotePage extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Add Note"),
+      ),
+      body: Center(
+        child: Text("Body of add note"),
+      )
+    );
+  }
+
+}
